bde737cc1237787fbdfa61e19ca258ad10fae054
Patricia Novaes <patricia.novaes@gmail.com>
2021-05-16 15:05:25 -0300

    Sync

---
+++ JS/main.js
@@ -0,0 +1,24 @@
+const countdown = () => {
+    const countDate = new Date("May 29, 2021 12:00:00").getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day) / hour);
+    const textMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute) / second);
+
+    document.querySelector(".day").innerText = textDay;
+    document.querySelector(".hour").innerText = textHour;
+    document.querySelector(".minute").innerText = textMinute;
+    document.querySelector(".second").innerText = textSecond;
+
+
+};
+
+setInterval(countdown, 1000);
